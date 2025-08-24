83a7ca714907ff26e9a59f944fa43bdf6747b0d0
Ericcccccheng <8199686@gmail.com>
2025-08-25 01:31:50 +1000

    vercel setting

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,9 @@
+export const API_BASE = (import.meta.env.VITE_API_BASE || '/').replace(/\/+$/, '') + '/';
+
+export async function apiGet(path: string, init: RequestInit = {}) {
+  const res = await fetch(API_BASE + path.replace(/^\/+/, ''), {
+    headers: { Accept: 'application/json' },
+    ...init
+  });
+  return res;
+}
